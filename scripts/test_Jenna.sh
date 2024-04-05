efdea5248989ed60f5eb8a46d6ec5dc45e356a31
rpw3mx <rpw3mx@hellbender-login.mgmt.hellbender>
2024-04-05 16:33:02 -0500

    saving Jenna's script

---
+++ scripts/test_Jenna.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+# test_Jenna.sh
+echo "=== start > test_Jenna.sh" $(date)
+
+echo "--- STATS input file: ${1}"
+
+SAMPLE_ID=$(cat $1 | grep ^FLT | awk '{print $2}')
+
+NUM_HETS=$(cat $1 | grep ^FLT | awk '{print $7}')
+
+echo "SAMPLE ID: ${SAMPLE_ID} has Hets ${NUM_HETS}"
+
+echo "=== end > test_Jenna.sh" $(date)
