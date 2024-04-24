bc067d3e08507a1a32f76f3a35786670a875390c
Rebekah Wilkinson - Hellbender <rpw3mx@umsystem.edu>
2024-04-24 15:55:10 -0500

    chrom 1 number of SNPs

--- scripts/test_Jenna.sh
+++ scripts/test_Jenna.sh
@@ -4,10 +4,12 @@ echo "=== start > test_Jenna.sh" $(date)
 
 echo "--- STATS input file: ${1}"
 
-SAMPLE_ID=$(cat $1 | grep ^FLT | awk '{print $2}')
+# SAMPLE_ID=$(cat $1 | grep ^FLT | awk '{print $2}')
 
-NUM_HETS=$(cat $1 | grep ^FLT | awk '{print $7}')
+# NUM_HETS=$(cat $1 | grep ^FLT | awk '{print $7}')
 
-echo "SAMPLE ID: ${SAMPLE_ID} has Hets ${NUM_HETS}"
+# echo "SAMPLE ID: ${SAMPLE_ID} has Hets ${NUM_HETS}"
 # rebekah's stuff goes here
+NUM_SNPs=$(cat $1 | grep "SNPs:" | awk -F '\t' '{ print $3,$4 }')
+echo "test :" $NUM_SNPs
 echo "=== end > test_Jenna.sh" $(date)
