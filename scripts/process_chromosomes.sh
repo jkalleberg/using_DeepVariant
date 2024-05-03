115bf44ca8f5a9fd16f97cb5eae53b39138499a9
Rebekah Wilkinson - Hellbender <rpw3mx@umsystem.edu>
2024-05-03 16:00:18 -0500

    automating bcftools stats all chromosomes

--- scripts/process_chromosomes.sh
+++ scripts/process_chromosomes.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # process_chromosomes.sh
 echo "=== start > process_chromosomes.sh" $(date)
-for chr in $(seq 1 5); do
+for chr in $(seq 1 29); do
     echo "processing chromosomes ${chr}"
     bash scripts/test_Jenna.sh chrom_$chr
 done
